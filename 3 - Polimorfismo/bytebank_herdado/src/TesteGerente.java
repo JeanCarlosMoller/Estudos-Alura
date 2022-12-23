e11ba99da29ce3f8e48e9c3fab6e525784a6f9ba
Jean Carlos Möller <jeancmoller17@gmail.com>
2022-12-23 15:58:34 -0300

    dando continuidade com os estudos

--- 3 - Polimorfismo/bytebank_herdado/src/TesteGerente.java
+++ 3 - Polimorfismo/bytebank_herdado/src/TesteGerente.java
@@ -1,19 +1,21 @@
 public class TesteGerente {
     public static void main(String[] args) {
+
+        Autenticavel referencia = new Gerente();
+
         Gerente gerente1 = new Gerente();
         gerente1.setNome("Ronaldo");
         gerente1.setCpf("111.222.333-55");
         gerente1.setSalario(5000.0);
-        // gerente1.setSenha(2222);
 
         System.out.println("---------Informações Gerente---------" + "\n" +
                 "Nome Gerente    : " + gerente1.getNome() + "\n" +
                 "CPF Gerente     : " + gerente1.getCpf() + "\n" +
                 "Salário Gerente : " + gerente1.getSalario() + "\n" +
                 "-------------------------------------");
-
-        // boolean autentica = gerente1.autentica(2222);
-        // System.out.println(autentica);
+        gerente1.setSenha(2222);
+        boolean autentica = gerente1.autentica(2222);
+        System.out.println(autentica);
 
         System.out.println(gerente1.getBonificacao());
     }
