9878745bd5cc190e43105928c2ee3e23899d04e5
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-03-20 09:55:04 -0300

    Dando continuidade com estudo. Pacote java.lang e classe Object

---
+++ 3 - Polimorfismo/bytebank_herdado_conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,16 @@
+
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+        String nome = "Alura";
+
+        String outra = nome.replace("A", "a");
+        // nome.toLowerCase();
+
+        System.out.println(nome);
+        System.out.println(outra);
+
+    }
+}
