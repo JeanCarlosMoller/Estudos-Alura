de91ae7c5222b22764f65893c912336ab3f782a4
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-04-06 00:47:22 -0300

    Requisições PUT com a anotação @PutMapping e Delete com @DeleteMapping, mapeando parâmetros dinâmicos em URL com a anotação @PathVariable, atualĩzação de informação de nova coluna no banco de dados e implementado conceito de exclusão lógica com uso de atributo booleano

--- Estudo API Rest - Java - Spring Boot/api/src/main/java/med/voll/api/paciente/Paciente.java
+++ Estudo API Rest - Java - Spring Boot/api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -25,11 +25,31 @@ public class Paciente {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Paciente(DadosCadastroPaciente dadosPaciete){
+        this.ativo = true;
         this.nome = dadosPaciete.nome();
         this.email = dadosPaciete.email();
         this.telefone = dadosPaciete.telefone();
         this.cpf = dadosPaciete.cpf();
         this.endereco = new Endereco(dadosPaciete.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPacientes dados) {
+    if(dados.nome() != null){
+        this.nome = dados.nome();
+    }
+    if(dados.telefone() != null){
+        this.telefone = dados.telefone();
+    }
+    if(dados.endereco() != null){
+        this.endereco.atualizarInformacoes(dados.endereco());
+    }
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
