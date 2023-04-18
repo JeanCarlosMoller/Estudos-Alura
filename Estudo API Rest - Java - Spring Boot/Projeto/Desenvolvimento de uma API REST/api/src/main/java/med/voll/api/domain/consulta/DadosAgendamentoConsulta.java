561edb602b3178ef045dbb85ad0432e6ac82cb2d
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-04-18 14:15:54 -0300

    Implantando nova funciuonalidade no projeto para agendamento e cancelamento de consultas. Consulta JPQL em uma interface repository, utilizando anotação @Query para consulta diretamente no banco

--- Estudo API Rest - Java - Spring Boot/Projeto/Desenvolvimento de uma API REST/api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
+++ Estudo API Rest - Java - Spring Boot/Projeto/Desenvolvimento de uma API REST/api/src/main/java/med/voll/api/domain/consulta/DadosAgendamentoConsulta.java
@@ -1,18 +1,23 @@
 package med.voll.api.domain.consulta;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import med.voll.api.domain.medico.Especialidade;
 
 import java.time.LocalDateTime;
 
 
 public record DadosAgendamentoConsulta(
-    Long idMedico,
+        Long idMedico,
 
-    @NotNull
+        @NotNull
     Long idPaciente,
 
-    @NotNull
+        @NotNull
     @Future
-    LocalDateTime data) {
+    //@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
+    LocalDateTime data,
+
+    Especialidade especialidade) {
 }
