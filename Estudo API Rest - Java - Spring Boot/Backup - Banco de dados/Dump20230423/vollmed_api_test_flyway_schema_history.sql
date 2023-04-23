c2180f4544da8eedbe2866046db558cae5c2e869
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-04-23 02:10:05 -0300

    Build da aplicação. Utilização de variáveis de ambiente no application-prod.properties para informações sensíveis.

---
+++ Estudo API Rest - Java - Spring Boot/Backup - Banco de dados/Dump20230423/vollmed_api_test_flyway_schema_history.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: vollmed_api_test
+-- ------------------------------------------------------
+-- Server version	8.0.31
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `flyway_schema_history`
+--
+
+DROP TABLE IF EXISTS `flyway_schema_history`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `flyway_schema_history` (
+  `installed_rank` int NOT NULL,
+  `version` varchar(50) DEFAULT NULL,
+  `description` varchar(200) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `script` varchar(1000) NOT NULL,
+  `checksum` int DEFAULT NULL,
+  `installed_by` varchar(100) NOT NULL,
+  `installed_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `execution_time` int NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  PRIMARY KEY (`installed_rank`),
+  KEY `flyway_schema_history_s_idx` (`success`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `flyway_schema_history`
+--
+
+LOCK TABLES `flyway_schema_history` WRITE;
+/*!40000 ALTER TABLE `flyway_schema_history` DISABLE KEYS */;
+INSERT INTO `flyway_schema_history` VALUES (1,'1','create-table-medicos','SQL','V1__create-table-medicos.sql',-996076185,'root','2023-04-22 21:33:25',18,1),(2,'2','alter-table-medicos-add-column-telefone','SQL','V2__alter-table-medicos-add-column-telefone.sql',-1848389755,'root','2023-04-22 21:33:25',14,1),(3,'3','create-table-pacientes','SQL','V3__create-table-pacientes.sql',829440318,'root','2023-04-22 21:33:25',16,1),(4,'4','alter-table-medicos-add-column-ativo','SQL','V4__alter-table-medicos-add-column-ativo.sql',1410818282,'root','2023-04-22 21:33:25',9,1),(5,'5','alter-table-pacientes-add-column-ativo','SQL','V5__alter-table-pacientes-add-column-ativo.sql',103283871,'root','2023-04-22 21:33:25',11,1),(6,'6','create-table-usuarios','SQL','V6__create-table-usuarios.sql',499780275,'root','2023-04-22 21:33:25',22,1),(7,'7','create-table-consultas','SQL','V7__create-table-consultas.sql',-159946368,'root','2023-04-22 21:33:25',38,1),(8,'8','create-table-consultas-add-column-motivo-cancelamento','SQL','V8__create-table-consultas-add-column-motivo-cancelamento.sql',1437665980,'root','2023-04-22 21:33:25',31,1);
+/*!40000 ALTER TABLE `flyway_schema_history` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-23  2:05:55
