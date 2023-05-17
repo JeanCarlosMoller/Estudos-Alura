bd07ceb6e38f9c0c7c154923d8c0753194820269
Jean Carlos Möller <jeancmoller17@gmail.com>
2023-05-16 22:21:28 -0300

    finalizando curso. Implementando a remoção e edição de empresas e como realizar o deploy da aplicação

--- Criando uma aplicação Java Web com Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ Criando uma aplicação Java Web com Servlet/gerenciador/src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -1,22 +1,24 @@
 package br.com.alura.gerenciador.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class NovaEmpresaServlet
+ */
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	
-
+	private static final long serialVersionUID = 1L;
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
 			throws ServletException, IOException {
 		System.out.println("Cadastrando nova empresa");
@@ -43,7 +45,7 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		response.sendRedirect("listaEmpresas");
 		
-//		//chamar o JSP
+//		//chamar o JSP ou Servlet
 //		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
 //		request.setAttribute("empresa", empresa.getNome());
 //		rd.forward(request, response);
